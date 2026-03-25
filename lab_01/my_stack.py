cdb22cca12d2fa7a4cf9eae60ae31106632aebab
MaxMobile-ru <109756598+MaxMobile-ru@users.noreply.github.com>
2026-03-25 10:36:39 +0300

    Update my_stack.py

--- lab_01/my_stack.py
+++ lab_01/my_stack.py
@@ -5,7 +5,10 @@ class Stack:
     self.items.append (item)
   def pop (self):
     if not self.isempty:
-      
-    return self.items.pop ()
+      return self.items.pop ()
+    else:
+      return 'Стек пуст'
+  def peek (self):
+    return self.items [-1]
   def isempty (self):
     return len (self.items) == 0
