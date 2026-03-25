0a881c60d21c6d55c89949fe7f120dc01f6b423c
MaxMobile-ru <109756598+MaxMobile-ru@users.noreply.github.com>
2026-03-25 10:39:50 +0300

    Update main.py

--- lab_01/main.py
+++ lab_01/main.py
@@ -5,3 +5,9 @@
 Проверка тегов HTML на корректность вложенности
 (<div><p></p></div>).
 '''
+import my_stack
+
+test_st = Stack ()
+test_st.push ('hello!')
+test_st.push ('goodbye!')
+print (test_st.peek ())
