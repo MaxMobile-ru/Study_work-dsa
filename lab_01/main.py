91c9b5aa8b9408ba47d9fa04901ccdb152cc12da
MaxMobile-ru <109756598+MaxMobile-ru@users.noreply.github.com>
2026-04-08 11:02:06 +0300

    Update main.py

--- lab_01/main.py
+++ lab_01/main.py
@@ -7,23 +7,42 @@
 '''
 from my_stack import *
 
+def process_string (i, value):
+    entire = ''
+    while value [i] != '>':
+        entire += value [i]
+        i += 1
+    return entire + '>', i + 1
+
+def check_stack (stack, entire):
+    if entire [2:-1] == stack.peek () [1:-1]:
+        stack.pop ()
+        return stack, True
+    else:
+        return stack, False
+
 test_value = '<div><p></p></div>'
 correct_stack = True
 stack01 = Stack ()
 new_entire = ''
-for i in range (len (test_value)):
+i = 0
+while True:
+    if i >= len (test_value) or i == -1:
+        break
     if test_value [i] == '<':
-        new_entire += test_value [i]
-    elif test_value [i] == '>':
-        new_entire += test_value [i]
-        if '/' in new_entire:
-            if new_entire [2:-1] == stack01.peek () [1:-1]:
-                stack01.pop ()
-            else:
-                
-        stack01.push (new_entire)
-        new_entire = ''
-    elif len (new_entire) != 0:
-        new_entire += test_value [i]
+        new_entire, i = process_string (i, test_value)
+        if not ('/' in new_entire):
+            stack01.push (new_entire)
+            new_entire = ''
+        elif '/' in new_entire:
+            stack01, correct_stack = check_stack (stack01, new_entire)
+            new_entire = ''
+    else:
+        i = test_value.find ('<', i, -1)
+    if not correct_stack:
+        break
 
-print (stack01.items)
+if correct_stack:
+    print ('Теги HTML вложены корректно.')
+else:
+    print ('Теги HTML вложены некорректно.')
