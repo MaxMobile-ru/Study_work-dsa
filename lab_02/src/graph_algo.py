76cc49385b05db6884c883a8b5241bfedb7e7da0
MaxMobile-ru <109756598+MaxMobile-ru@users.noreply.github.com>
2026-04-27 19:01:52 +0300

    Update graph_algo.py

--- lab_02/src/graph_algo.py
+++ lab_02/src/graph_algo.py
@@ -1,1 +1,14 @@
 # Реализация графов
+def generate_graph (n, lst_ribs):
+  matrix_rel = []
+  for _ in range (n):
+    tmp = []
+    for _ in range (n):
+      tmp.append (0)
+    matrix_rel.append (tmp)
+  for item in lst_ribs:
+    start = item [0]
+    end = item [1]
+    matrix_rel [start - 1] [end - 1] = 1
+    matrix_rel [end - 1] [start - 1] = 1
+  return matrix_rel
