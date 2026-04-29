fed7bc6dae93481bb373ed9b231c6f503094b4f6
MaxMobile-ru <109756598+MaxMobile-ru@users.noreply.github.com>
2026-04-29 20:50:45 +0300

    Update tree_algo.py

--- lab_02/src/tree_algo.py
+++ lab_02/src/tree_algo.py
@@ -10,24 +10,44 @@ class Node:
     self.left = Node (item)
 
 def new_node_bst (node:Node, item):
-    if item < node.value:
-        if node.left == None:
-            node.new_left (item)
-        else:
-            node.left = new_node_bst (node.left, item)
-    elif item > node.value:
-        if node.right == None:
-           node.new_right (item)
-        else:
-           node.right = new_node_bst (node.right, item)
-    return node
+  if item < node.value:
+    if node.left == None:
+      node.new_left (item)
+    else:
+      node.left = new_node_bst (node.left, item)
+  elif item > node.value:
+    if node.right == None:
+      node.new_right (item)
+    else:
+      node.right = new_node_bst (node.right, item)
+  return node
+
+def search_node_bst (node:Node, item):
+  if item == node.value:
+    return str (node.value)
+  elif item < node.value:
+    if node.left == None:
+      return '\nЧисло не найдено'
+    else:
+      return str (node.left.value) + '\\' + search_node_bst (node.left, item)
+  elif item > node.value:
+    if node.right == None:
+      return '\nЧисло не найдено'
+    else:
+      return str (node.right.value) + '\\' + search_node_bst (node.right, item)
 
 class BST:
   def __init__ (self):
     self.root = None
 
-  def new_nod (self, item):
+  def new_node (self, item):
     if self.root == None:
       self.root = Node (item)
     else:
       self.root = new_node_bst (self.root, item)
+
+  def search_nod (self, item):
+    if self.root == None:
+      return 'Бинарное дерево пусто'
+    else:
+      path = '\\' + search_nod (self.root, item)
