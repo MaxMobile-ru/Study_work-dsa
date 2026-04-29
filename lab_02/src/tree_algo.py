4b9614bd06495ee56a5b2d4a9715591a3ca1850f
MaxMobile-ru <109756598+MaxMobile-ru@users.noreply.github.com>
2026-04-29 20:26:51 +0300

    Update tree_algo.py

--- lab_02/src/tree_algo.py
+++ lab_02/src/tree_algo.py
@@ -1,28 +1,35 @@
 # Реализация деревьев и heap sort
+class Node:
+  def __init__ (self, item):
+    self.value = item
+    self.right = None
+    self.left = None
+  def new_right (self, item):
+    self.right = Node (item)
+  def new_left (self, item):
+    self.left = Node (item)
+
+def new_node_bst (node:Node, item):
+    if item < node.value:
+        if node.left == None:
+            node.new_left (item)
+        else:
+            node.left = new_node_bst (node.left, item)
+    elif item > node.value:
+        if node.right == None:
+           node.new_right (item)
+        else:
+           node.right = new_node_bst (node.right, item)
+    return node
+
+
+
 class BST:
   def __init__ (self):
     self.root = None
-    self.right = None
-    self.left = None
 
   def new_nod (self, item):
     if self.root == None:
-      self.root = item
-    elif item > self.root:
-      if self.right == None:
-        self.right = item
-      else:
-        self.right.new_nod (item)
-    elif item < self.root:
-      if self.left == None:
-        self.left = item
-      else:
-        self.left.new_nod (item)
-
-  def search_nod (self, item, path='\\'):
-    if self.root == item:
-      return path + str (item)
-    elif item < self.root:
-      return self.left.search_nod (item, path + str (item) + '\\')
-    elif item > self.root:
-      return self.right.search_nod (item, path + str (item) + '\\')
+      self.root = Node (item)
+    else:
+      self.root = new_node_bst (self.root, item)
