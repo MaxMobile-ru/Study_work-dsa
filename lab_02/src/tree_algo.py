f10b5f85f14de817e228b3d2995b9ab24bc9d569
MaxMobile-ru <109756598+MaxMobile-ru@users.noreply.github.com>
2026-04-29 18:37:55 +0300

    Update tree_algo.py

--- lab_02/src/tree_algo.py
+++ lab_02/src/tree_algo.py
@@ -12,17 +12,17 @@ class BST:
       if self.right == None:
         self.right = item
       else:
-        new_nod (self.right, item)
+        self.new_nod (self.right, item)
     elif item < self.root:
       if self.left == None:
         self.left = item
       else:
-        new_nod (self.left, item)
+        self.new_nod (self.left, item)
 
   def search_nod (self, item, path='\\'):
     if self.root == item:
       return path + str (item)
     elif item < self.root:
-      return search_nod (self.left, item, path + str (item) + '\\')
+      return self.search_nod (self.left, item, path + str (item) + '\\')
     elif item > self.root:
-      return search_nod (self.right, item, path + str (item) + '\\')
+      return self.search_nod (self.right, item, path + str (item) + '\\')
