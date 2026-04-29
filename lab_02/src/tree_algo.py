d4875fca0ae1b69eabe009db24c0a5f9bf35f3c5
MaxMobile-ru <109756598+MaxMobile-ru@users.noreply.github.com>
2026-04-29 18:35:06 +0300

    Update tree_algo.py

--- lab_02/src/tree_algo.py
+++ lab_02/src/tree_algo.py
@@ -1,1 +1,28 @@
 # Реализация деревьев и heap sort
+class BST:
+  def __init__ (self):
+    self.root = None
+    self.right = None
+    self.left = None
+
+  def new_nod (self, item):
+    if self.root == None:
+      self.root = item
+    elif item > self.root:
+      if self.right == None:
+        self.right = item
+      else:
+        new_nod (self.right, item)
+    elif item < self.root:
+      if self.left == None:
+        self.left = item
+      else:
+        new_nod (self.left, item)
+
+  def search_nod (self, item, path='\'):
+    if self.root == item:
+      return path + str (item)
+    elif item < self.root:
+      return search_nod (self.left, item, path + str (item) + '\\')
+    elif item > self.root:
+      return search_nod (self.right, item, path + str (item) + '\\')
