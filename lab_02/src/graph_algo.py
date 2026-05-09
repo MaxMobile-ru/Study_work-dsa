29fb64f8069f76d40d30642610b3e803633fd471
MaxMobile-ru <109756598+MaxMobile-ru@users.noreply.github.com>
2026-05-09 22:07:35 +0300

    Update graph_algo.py

--- lab_02/src/graph_algo.py
+++ lab_02/src/graph_algo.py
@@ -25,38 +25,32 @@ def matrix_to_dict (matrix):
 
 def relations_search (graph):
   graph = matrix_to_dict (graph)
-  res = []
-  queue = []
-  is_visited_lst = []
+  is_visited_lst = [None]
   for _ in range (len (graph.keys ())):
     is_visited_lst.append (False)
+  queue = []
   i = 0
-  current = 0
+  res = list ()
+  way = set ()
   while False in is_visited_lst:
     if len (queue) == 0:
-      is_visited_lst [current] = True
+      current = is_visited_lst.index (False)
       queue.append (current)
-    else:
-      i += 1
-      if i == len (queue):
-        res.append (set (queue))
-        current = is_visited_lst.index (False)
-        i = 0
-        queue = []
-        continue
+      is_visited_lst [current] = True
+      continue
     current = queue [i]
+    way.add (current)
+    is_visited_lst [current] = True
     for item in graph [current]:
       if not (item in set (queue)):
-        is_visited_lst [item] = True
         queue.append (item)
-        if not (False in is_visited_lst):
-          res.append (set (queue))
-    if len (graph [current]) == 0:
-      res.append (set (queue))
-      if False in is_visited_lst:
-        current = is_visited_lst.index (False)
-        i = 0
-        queue = []
+    if i + 1 == len (queue):
+      queue = []
+      res.append (way)
+      way = set ()
+      i = 0
+    else:
+      i += 1
   return res
 
 def correct_print_search (graph):
